9b240155f42989ea394ef3f481ce21a527acb936
Ochiroo <ochiroo.beatz@gmail.com>
2020-02-20 17:42:52 +0100

    toglogchiin eeljiig solidog bolgoson

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //  toglogchiin eelj
-
-var activePlayer = 1;
+var activePlayer = 0;
 
 // toglogchiin tugluulsan onoo
 
@@ -19,11 +18,12 @@ document.getElementById('score-1').textContent = 0;
 document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
 
-var diceDom = document.querySelector('.dice');
+// shoonii zurag heseg
 
+var diceDom = document.querySelector('.dice');
 diceDom.style.display = "none";
 
-// shoonii event
+// shoog shideh event
 document.querySelector('.btn-roll').addEventListener('click', function () {
     // shoonii ali talaaraa buusan eseh
     var diceNumber = Math.floor(Math.random() * 6) + 1;
@@ -31,4 +31,20 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     // shoonii zurag heseg
     diceDom.style.display = "block";
     diceDom.src = 'dice-' + diceNumber + '.png';
+
+    // toglogchiin eeljiin onoog gargah
+    if (diceNumber !== 1) {
+        roundScore = roundScore + diceNumber
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    } else {
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+        roundScore = 0;
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    alert('darlaa')
 });
