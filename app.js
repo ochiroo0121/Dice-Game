dd3cbce7221c826e061fff5671049b7d67df77ad
Ochiroo <ochiroo.beatz@gmail.com>
2020-02-20 18:36:20 +0100

    hold tovchiig ajilladag bolloo

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ document.getElementById('current-1').textContent = 0;
 var diceDom = document.querySelector('.dice');
 diceDom.style.display = "none";
 
+function switchToNextPlayer() {
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+}
+
 // shoog shideh event
 document.querySelector('.btn-roll').addEventListener('click', function () {
     // shoonii ali talaaraa buusan eseh
@@ -37,14 +45,28 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         roundScore = roundScore + diceNumber
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
-        document.getElementById('current-' + activePlayer).textContent = 0;
-        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-        roundScore = 0;
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
+        switchToNextPlayer()
     }
 });
 
+// hold tovchnii event
 document.querySelector('.btn-hold').addEventListener('click', function () {
-    alert('darlaa')
+
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+
+    if (scores[activePlayer] >= 20) {
+        document.getElementById('name-' + activePlayer).textContent = 'WINNER!!!';
+    } else {
+        switchToNextPlayer();
+    }
+
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+
+    switchToNextPlayer()
 });
+
+// new game event
+// document.querySelector('.btn-new').addEventListener('click', function(){
+
+// })
